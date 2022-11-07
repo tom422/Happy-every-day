6d2b732a918be7c7b1ba126f32663bd6443d4e66
Your Name <you@example.com>
2022-11-07 16:37:06 +0800

    配置修改

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row class="mb-4">
       <el-button @click="handleRouter">Default</el-button>
-      <el-button type="primary">Primary</el-button>
+      <el-button @click="getData" type="primary">Primary</el-button>
       <el-button type="success">Success</el-button>
       <el-button type="info">Info</el-button>
       <el-button type="warning">Warning</el-button>
@@ -21,7 +21,13 @@ const handleRouter = () => {
 
 
 const getData = () => {
-  request
+  request({
+    method: 'get',
+    url: '/tool/file',
+  }).then(res=>{
+    console.log(res);
+    
+  })
 }
 </script>
 
