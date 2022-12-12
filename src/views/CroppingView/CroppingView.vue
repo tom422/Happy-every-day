0ab50435eb378c46b4dc597a325e8131b5e63840
Your Name <you@example.com>
2022-12-12 15:28:28 +0800

    图片裁剪

--- src/views/CroppingView/CroppingView.vue
+++ src/views/CroppingView/CroppingView.vue
@@ -1,37 +1,96 @@
 <template>
-    <div class="page-content">
-        
-       <div>
-            <img ref="image" src="@/assets/images/image.jpg"/>
-       </div>
-       <div>
-            <img :src="imageData"/>
-       </div>
-       <el-button @click="onFileSave">裁剪图片</el-button>
-    </div>
+     <div class="page-content">
+
+          <div class="image-box">
+               <img ref="image" src="@/assets/images/image.jpg" />
+          </div>
+          <div>
+               <el-input-number v-model="cropperWidth" :min="1" :max="2000" @change="setWidth" />
+               <el-input-number v-model="cropperHeight" :min="1" :max="2000" @change="setWidth" />
+               <el-button @click="setWidth">确认</el-button>
+
+               <div>
+                    <el-upload ref="uploadRef" class="upload-demo"
+                         action="" accept="image/*" :http-request="uploadPictures">
+                         <template #trigger>
+                              <el-button type="primary">
+                                   <el-icon><UploadFilled /></el-icon>
+                              </el-button>
+                         </template>
+                         <template #tip>
+                              <div class="el-upload__tip">
+                                   jpg/png files with a size less than 500kb
+                              </div>
+                         </template>
+                    </el-upload>
+               </div>
+          </div>
+          <el-button @click="onFileSave">裁剪图片</el-button>
+          <ImageDialog ref="imageVue"></ImageDialog>
+     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from '@vue/reactivity'
+import { ref, VueElement } from 'vue'
 import { onMounted } from '@vue/runtime-core'
 import 'cropperjs/dist/cropper.css'
 import Cropper from 'cropperjs';
+import ImageDialog from './imageDialog.vue';
+import { UploadFilled } from '@element-plus/icons-vue'
+import type { UploadInstance } from 'element-plus'
+
+const uploadRef = ref<UploadInstance>()
+
+const submitUpload = () => {
+  uploadRef.value!.submit()
+}
+
+const imageVue = ref<VueElement>()
 // const myClouds = ref(null)
 const image = ref(null)
 const imageData = ref('')
-let myCropper:Cropper
-onMounted(()=>{
-    myCropper = new Cropper(image.value as unknown as HTMLImageElement)
- })
+let myCropper: Cropper
+onMounted(() => {
+     myCropper = new Cropper(image.value as unknown as HTMLImageElement)
+})
+// 设置宽高
+const cropperWidth = ref(100)
+const cropperHeight = ref(100)
+const setWidth = () => {
+     myCropper.setData({
+          width: cropperWidth.value,
+          height: cropperHeight.value
+     })
+}
+/** 上传图片 */
+const uploadPictures = (UploadRequestOptions: any) => {
+     console.log(UploadRequestOptions);
+     const file = UploadRequestOptions.file
+     if (/^image\/\w+$/.test(file.type)) {
+          const blobURL = URL.createObjectURL(file);
+          myCropper.replace(blobURL)
+          myCropper.reset()
+          // URL.revokeObjectURL(blobURL);
+     } else {
+          // showMessage('Please choose an image file.');
+     }
+}
 
+/** 裁剪图片 */
+const onFileSave = () => {
+     const data = myCropper.getCroppedCanvas({
+          imageSmoothingQuality: 'high'
+     }).toDataURL('image/jpeg')
+     console.log(imageVue.value);
+     const dialogImage = imageVue.value as any
+     dialogImage.open(data)
 
- const onFileSave = ()=>{
-    imageData.value = myCropper.getCroppedCanvas({
-        imageSmoothingQuality: 'high'
-      }).toDataURL('image/jpeg')
- }
+}
 
 </script>
 
 <style lang="scss" scoped>
+.image-box{
+     max-height: 500px;
+}
 </style>
--- src/views/CroppingView/imageDialog.vue
+++ src/views/CroppingView/imageDialog.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <el-dialog v-model="dialogVisible" title="Tips" width="30%" :before-close="handleClose">
-            <span>This is a message</span>
+        <el-dialog v-model="dialogVisible" title="图片" width="50%" :before-close="handleClose">
+            <div class="imgbox">
+                <img :src="imgData" />
+            </div>
             <template #footer>
                 <span class="dialog-footer">
-                    <el-button @click="dialogVisible = false">Cancel</el-button>
-                    <el-button type="primary" @click="dialogVisible = false">
-                        Confirm
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="imgDowload">
+                        下载
                     </el-button>
                 </span>
             </template>
@@ -15,22 +17,41 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, toRef } from 'vue'
 import { ElMessageBox } from 'element-plus'
+import { fileSave } from '@/tool/file';
 
 const dialogVisible = ref(false)
+const imgData = ref('')
+
+const open = (base='')=>{
+    dialogVisible.value = true
+    imgData.value = base
+}
 
 const handleClose = (done: () => void) => {
-  ElMessageBox.confirm('Are you sure to close this dialog?')
-    .then(() => {
-      done()
-    })
-    .catch(() => {
-      // catch error
-    })
+//   ElMessageBox.confirm('Are you sure to close this dialog?')
+//     .then(() => {
+//       
+//     })
+//     .catch(() => {
+//       // catch error
+//     })
+    done()
 }
+const imgDowload = ()=>{
+    fileSave(imgData.value,'image.jpg')
+}
+defineExpose({
+    open
+})
 </script>
 
 <style lang="scss" scoped>
-
+.imgbox{
+    img{
+        width: 100%;
+        max-height: 800px;
+    }
+}
 </style>
