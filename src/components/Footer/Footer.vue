7f0a952cc0965e5d6483c7b1449d773ac9ea36e2
qinxuanlong <1470681411@qq.com>
2023-05-31 15:55:50 +0800

    开始

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,65 +1,72 @@
 <template>
-    <div class="footer-area">
-        <!-- FOOTER START -->
-        <el-row class="">
-            <el-col :span="20" :offset="2">
-                <div>
-                    <div class="tp-border"></div>
-                    <div class="footer_bottom ">
-
-                        <el-row>
-                            <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="24">
-
-                                <p>Copyright &copy; 2021.Company name All rights reserved.<a target="_blank"
-                                        href="https://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a>
-                                </p>
-
-                            </el-col>
-                            <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="24">
-                                <div class="footer_right">
-                                    <span>Follow us</span>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    <a href="#"><i class="fab fa-vimeo-v"></i></a>
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                </div>
-
-
-                            </el-col>
-                        </el-row>
-
-                    </div>
+  <div class="footer-area">
+    <!-- FOOTER START -->
+    <el-row class="">
+      <el-col :span="20" :offset="2">
+        <div>
+          <div class="tp-border"></div>
+          <div class="footer_bottom">
+            <el-row>
+              <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="24">
+                <div class="footer-record">
+                  <span>
+                    <a
+                      class="footer-link"
+                      href="https://beian.miit.gov.cn/"
+                      target="_blank"
+                      >皖ICP备2023004430号
+                    </a>
+                  </span>
                 </div>
-            </el-col>
-        </el-row>
+              </el-col>
+              <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="24">
+                <div class="footer_right">
+                  <a
+                    href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                  </a>
+                  <img src="../../assets/images/gaba.png" />
+                  皖公网安备 42018502003215号
+                </div>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
 
-        <!-- FOOTER END -->
-    </div>
+    <!-- FOOTER END -->
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .footer-area {
-     /* background: #f9fdfa; */
-    padding-top: 40px ;
-    padding-top: 20px ;
- }
-
- .tp-border {
-    border-top: 1px solid #e4ecee;
+  /* background: #f9fdfa; */
+  padding-top: 40px;
+  padding-top: 20px;
 }
 
- .footer_bottom {
-     color: var(--fontColor);
-     padding: 20px 0;
-     vertical-align: middle;
- }
+.tp-border {
+  border-top: 1px solid #e4ecee;
+}
 
- .footer_right {
-     /* line-height: 40px; */
-     vertical-align: middle;
- }
+.footer_bottom {
+  color: var(--fontColor);
+  padding: 20px 0;
+  vertical-align: middle;
+}
+
+.footer_right {
+  /* line-height: 40px; */
+  
+  text-align: right;
+}
+
+.footer_right  img {
+    vertical-align: middle;
+}
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,31 +1,31 @@
 <template>
-    <div>
-        <canvas class="myClouds" height="240" ref="myClouds"></canvas>
-        <HeaderVue></HeaderVue>
+  <div>
+    <HeaderVue></HeaderVue>
+        <div class="page-content">
             <router-view />
-        <FooterVue></FooterVue>
-    </div>
+        </div>
+    <FooterVue></FooterVue>
+  </div>
 </template>
 
 <script setup lang="ts">
-
-import * as klouds from 'klouds'   
-import { Get } from '@/api/requerst';
-import { onMounted, ref } from '@vue/runtime-core';
-import HeaderVue from '@/components/Header/PageHeader.vue';
-import FooterVue from '@/components/Footer/Footer.vue';
+// import * as klouds from 'klouds'
+import { Get } from '@/api/requerst'
+import { onMounted, ref } from '@vue/runtime-core'
+import HeaderVue from '@/components/Header/PageHeader.vue'
+import FooterVue from '@/components/Footer/Footer.vue'
 const myClouds = ref(null)
-onMounted(()=>{
-    const canvas = myClouds.value as unknown as HTMLCanvasElement
-    const instance = klouds.create({
-        selector:canvas,
-        layerCount: 8,
-        speed: 1,
-    });
-    instance.setCloudColor1('#1ab3cc');
-    instance.setCloudColor2('#ffffff');
-    instance.setBgColor('#fff');
-    instance.start()
+onMounted(() => {
+  // const canvas = myClouds.value as unknown as HTMLCanvasElement
+  // const instance = klouds.create({
+  //     selector:canvas,
+  //     layerCount: 8,
+  //     speed: 1,
+  // });
+  // instance.setCloudColor1('#1ab3cc');
+  // instance.setCloudColor2('#ffffff');
+  // instance.setBgColor('#fff');
+  // instance.start()
 })
 // Get('/tool/login').then((res)=>{
 //     console.log(res);
@@ -34,13 +34,16 @@ onMounted(()=>{
 </script>
 
 <style scoped>
+.myClouds {
+  position: fixed;
+  left: 0;
+  top: 60px;
+  width: 100%;
+  max-height: 300px !important;
+  z-index: -1;
+}
 
-.myClouds{
-    position: fixed;
-    left: 0;
-    top: 60px;
-    width: 100%;
-    max-height: 300px !important;
-    z-index: -1;
+.page-content{
+    min-height: 80vh;
 }
 </style>
